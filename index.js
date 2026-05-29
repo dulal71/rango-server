080a38950ff0566dae55cc7fcb1e5986e1273886
dulal71 <ahmeddulal4211@gmail.com>
2026-05-29 23:56:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors=require('cors')
+const dotenv=require('dotenv')
+const app = express();
+const port =process.env.PORT || 5000;
+dotenv.config()
+app.use(express.json())
+app.use(cors())
+const uri=process.env.MONGODB_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const db=client.db('rango-data')
+    const collection=db.collection('products')
+   
+    await client.connect();
+
+    // get all products
+  app.get('/products',async(req,res)=>{
+    try{
+      const page = req.query.page
+      const search = req.query.search
+      const category = req.query.category;
+      const brand = req.query.brand;
+      const color = req.query.color;
+      console.log(category);
+      console.log(brand);
+      console.log(color);
+      
+      let query={}
+      if (search) {
+  query.name = { $regex: search, $options: 'i' };
+}
+console.log(search);
+if (category) {
+  query.category = category;
+}
+if (brand) {
+      query.brand = brand;
+    }
+      const limit=12
+      const skip=(page-1)* limit
+
+        const cursor=await collection.find(query).skip(skip).limit(limit)
+        const result=await cursor.toArray()
+res.send(result)
+    }catch(error){
+     res.status(500).send({
+        success:false,
+        message:'Internal Server Error'
+     })   
+    }
+  })
+    // get top selling product
+  app.get('/topProducts',async(req,res)=>{
+    try{
+        const cursor=await collection.find().sort({rating:-1}).limit(6)
+        const result=await cursor.toArray()
+res.send(result)
+    }catch(error){
+     res.status(500).send({
+        success:false,
+        message:'Internal Server Error'
+     })   
+    }
+  })
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
